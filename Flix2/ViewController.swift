956a5c13d0399fb8cf07b415c9514bcdd38e82a1
Tasha <tashaip77@gmail.com>
2020-09-23 14:45:55 -0700

    Initial Commit

---
+++ Flix2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix2
+//
+//  Created by Tasha Ip Ying Ler on 9/23/20.
+//  Copyright © 2020 developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
