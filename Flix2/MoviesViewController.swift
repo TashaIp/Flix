718ae75fdeab28cdda67dad85f4e6f18ff02a896
Tasha <tashaip77@gmail.com>
2020-09-23 23:58:39 -0700

    table view done

--- Flix2/MoviesViewController.swift
+++ Flix2/MoviesViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    
+    
     @IBOutlet var tableView: UITableView!
     var movies = [[String:Any]]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -24,6 +27,7 @@ class MoviesViewController: UIViewController {
            } else if let data = data {
               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
             self.movies = dataDictionary["results"] as! [[String:Any]]
+            self.tableView.reloadData()
                print(dataDictionary)
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
@@ -33,16 +37,18 @@ class MoviesViewController: UIViewController {
         }
         task.resume()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        cell.textLabel?.text = title
+        return cell
+    }
+
+    
 
 }
